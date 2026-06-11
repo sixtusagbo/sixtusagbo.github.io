a14466f89f86ec7a1c5f8513d7b3c56d818927b1
Sixtus Agbo <miracleagbosixtus@gmail.com>
2026-06-11 08:01:02 +0100

    Remove unused UI components

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ProjectCard } from "./ProjectCard";
-export { default as ExperienceCard } from "./ExperienceCard";
-export { default as SocialLinks } from "./SocialLinks";
-export { default as SkillBadge } from "./SkillBadge";
